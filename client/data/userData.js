a26a6126e71592567d1e7085d5a9658239af6cad
Nivedita Tripathi <niveditat183@gmail.com>
2025-02-06 14:07:02 +0530

    first commit

---
+++ client/data/userData.js
@@ -0,0 +1,10 @@
+export const userData = {
+  _id: 1,
+  profilePic: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+  name: "Nivedita",
+  email: "me@t.com",
+  password: "1234567890",
+  address: "somewhere on earth",
+  city: "roorkee",
+  contact: "1234567890",
+};
